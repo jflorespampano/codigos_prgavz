459bedc1e9ec45a154718b9ab0c3e08f8c440cea
Jesús Alejandro Flores Hernández <jflorespampano@gmail.com>
2023-03-15 11:50:56 -0600

    documentacion de codigo de todos los ejemplos

--- vuejs/main.js
+++ vuejs/main.js
@@ -1,82 +1,64 @@
 //https://es.vuejs.org/v2/guide/forms.html
 const { createApp } = Vue
 
-const app=  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!',
-        count:0,
-        datosCargados: true,
-        estados:[
-            {id:"pu", nombre:"puebla"},
-            {id:"ca", nombre:"campeche"},
-            {id:"me", nombre:"merida"},
-        ], 
-        estadoSeleccionado:"ca",
-        albums:[
-            {
-              "userId": 1,
-              "id": 1,
-              "title": "quidem molestiae enim"
-            },
-            {
-              "userId": 1,
-              "id": 2,
-              "title": "sunt qui excepturi placeat culpa"
-            },
-            {
-              "userId": 1,
-              "id": 3,
-              "title": "omnis laborum odio"
-            },
-            {
-              "userId": 1,
-              "id": 4,
-              "title": "non esse culpa molestiae omnis sed optio"
-            },
-            {
-              "userId": 1,
-              "id": 5,
-              "title": "eaque aut omnis a"
-            },
-            {
-              "userId": 1,
-              "id": 6,
-              "title": "natus impedit quibusdam illo est"
-            },
-            {
-              "userId": 1,
-              "id": 7,
-              "title": "quibusdam autem aliquid et et quia"
-            },
-            {
-              "userId": 1,
-              "id": 8,
-              "title": "qui fuga est a eum"
-            },
-            {
-              "userId": 1,
-              "id": 9,
-              "title": "saepe unde necessitatibus rem"
-            },
-            {
-              "userId": 1,
-              "id": 10,
-              "title": "distinctio laborum qui"
-            }
-        ]
+const app = createApp({
+  data() {
+    return {
+      message: 'Cargando datos...!',
+      datosCargados: true,
+      albums: [],
+      post: {
+        id: '1',
+        userId: '2',
+        title: 'mi titulo'
       }
+    }
+  },
+  methods: {
+    agregarPost() {
+      //https://jsonplaceholder.typicode.com/albums
+      const datajson = JSON.stringify(app.post)
+      fetch('https://jsonplaceholder.typicode.com/albums', {
+        method: 'POST',
+        body: datajson,
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          app.datosCargados = true;
+          app.ponMensaje(`Dato: ${datajson} Agregado correctamente`);
+        });
     },
-    methods:{
-        increment(){
-            this.count++;
+    ponMensaje(m) {
+      app.message = m;
+      setTimeout(function () {
+        app.message = "";
+        app.datosCargados = false;
+      }, 3000);
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(function (data) {
+        if (data.ok) {
+          data.json().then(function (datosjson) {
+            app.albums = datosjson.slice(1, 10);
+            app.datosCargados = true;
+            app.ponMensaje("Datos  de la tabla recuperados correctamente");
+            console.log("datos cargados");
+          });
+        } else {
+          app.datosCargados = false;
+          app.ponMensaje("error en la respuesta del servidor 😢");
         }
-    },
-    mounted() {
-        // `this` refers to the component instance.
-        console.log(this.count) // => 1
-        // data can be mutated as well
-        this.count = 2
-      }
-  }).mount('#app')
+      })
+      .catch(function (error) {
+        app.datosCargados = false;
+        app.ponMensaje("error en la red 😢");
+      });
+  }
+}).mount('#app')
 
